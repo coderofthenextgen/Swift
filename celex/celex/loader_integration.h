dad19b0cb064f5b00c0874e8b907f2adb959705c
coderofthenextgen <coderofthenextgen@users.noreply.github.com>
2026-08-08 15:03:07 -0500

    cleanup: remove celex, exa, roblox-dumper-mac, .DS_Store, script.lua

--- celex/celex/loader_integration.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <atomic>
-
-// Global flag to control loader -> cheat transition
-namespace LoaderState
-{
-    inline std::atomic<bool> authenticated = false;
-    inline std::atomic<bool> should_exit_loader = false;
-}
--- exa/src/overlay.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-auto render_esp(float screen_w, float screen_h) -> void;
-auto render_player_list() -> void;
