7b87567f91d7d8d15759bbfed54eedc2dd6be5ae
coderofthenextgen <coderofthenextgen@users.noreply.github.com>
2026-08-19 19:18:49 -0500

    clean: remove old files, keep swift.lua + loader + places folder

--- loader.lua
+++ loader.lua
@@ -1,13 +1,1 @@
-local placeId = game.PlaceId
-local repo = 'https://raw.githubusercontent.com/coderofthenextgen/Swift-Hub/main/'
-local v = '?v=' .. tostring(os.time())
-
-local ok, res = pcall(function()
-    return game:HttpGet(repo .. 'places/' .. placeId .. '.lua' .. v)
-end)
-
-if ok and res and not res:find('404') then
-    loadstring(res)()
-else
-    loadstring(game:HttpGet(repo .. 'universal.lua' .. v))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/coderofthenextgen/Swift-Hub/main/swift.lua"))()
